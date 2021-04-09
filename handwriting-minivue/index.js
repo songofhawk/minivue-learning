33cfb67eb8956089e10282e729b104d42b69bbc3
songofhawk <songofhawk@gmail.com>
2021-04-09 17:25:16 +0800

    照抄B站"手写mini-vue"视频中的ref实现代码

---
+++ handwriting-minivue/index.js
@@ -0,0 +1,47 @@
+let currentEffect;
+
+class Dep {
+    // 1.收集依赖
+    constructor(val) {
+        this.effects = new Set()
+        this._val = val
+    }
+    get value(){
+        dep.depend()
+        return this._val
+    }
+    set value(newVal){
+        this._val = newVal
+        this.notice()
+    }
+    depend(){
+        if (currentEffect){
+            this.effects.add(currentEffect)
+        }
+    }
+    // 2.触发依赖
+    notice(){
+        // 触发之前收集到的依赖
+        this.effects.forEach((effect)=>{
+            effect()
+        })
+    }
+}
+
+const dep = new Dep(10)
+function effectWatch(effect){
+    currentEffect = effect
+    effect()
+    currentEffect = null
+}
+
+
+let b
+effectWatch(()=>{
+    console.log("heihei")
+    b = dep.value + 10
+})
+
+// 值发生变化
+dep.value = 20
+dep.notice()
